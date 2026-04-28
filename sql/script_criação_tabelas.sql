7c591eaa70ed8ccb55eefaee596a369db84fe78d
Adne Ronaldo <adneronaldo@gmail.com>
2026-04-27 23:55:13 -0300

    Carga das dimensões no banco

--- sql/script_criação_tabelas.sql
+++ sql/script_criação_tabelas.sql
@@ -4,49 +4,49 @@ default collate utf8mb4_0900_ai_ci;
 
 CREATE TABLE dim_orcamento (
     
-    sk_orcamento int(10) primary key not null,
+    sk_orcamento smallint primary key not null,
 
-    cod_funcao int(15) not null,
-    nome_funcao varchar(50) not null,
+    cod_funcao varchar(50) not null,
+    nome_funcao varchar(300) not null,
 
-    cod_subfuncao int(15) not null,
-    nome_subfuncao varchar(50) not null,
+    cod_subfuncao varchar(50) not null,
+    nome_subfuncao varchar(300) not null,
 
-    cod_programa int(15) not null,
-    nome_programa varchar(100) not null,
+    cod_programa varchar(50) not null,
+    nome_programa varchar(300) not null,
 
-    cod_acao int(15) not null,
-    nome_acao varchar(155) not null
+    cod_acao varchar(50) not null,
+    nome_acao varchar(300) not null
 
 );
 
 create table dim_despesa (
 
-    sk_despesa int(10) primary key not null,
+    sk_despesa smallint primary key not null,
 
-    cod_categoria_economica int(15) not null,
-    nome_categoria_economica varchar(29) not null,
+    cod_categoria_economica varchar(50) not null,
+    nome_categoria_economica varchar(250) not null,
 
-    cod_grupo_despesa int(15) not null,
-    nome_grupo_despesa varchar(56) not null,
+    cod_grupo_despesa varchar(50) not null,
+    nome_grupo_despesa varchar(250) not null,
 
-    cod_elemento_despesa int(15) not null,
-    nome_elemento_despesa varchar(65) not null,
+    cod_elemento_despesa varchar(50) not null,
+    nome_elemento_despesa varchar(250) not null,
 
-    cod_modalidade int(15) not null,
-    nome_modalidade varchar(82) not null
+    cod_modalidade varchar(50) not null,
+    nome_modalidade varchar(250) not null
 
 );
 
 create table dim_geografica (
-    sk_geo int(10) primary key not null,
+    sk_geo smallint primary key not null,
     uf char(2) not null,
     municipio varchar(100) not null,
-    tipo_gasto varchar(50) not null
+    tipo_gasto varchar(250) not null
 );
 
 create table dim_tempo (
-    sk_tempo tinyint not null,
+    sk_tempo smallint not null,
     ano tinyint not null,
     mes tinyint not null,
     trimestre tinyint not null,
@@ -55,21 +55,26 @@ create table dim_tempo (
 );
 
 create table dim_emenda (
-    sk_emenda int not null,
+    sk_emenda smallint not null,
     nome_autor varchar(150) not null,
-    numero_emenda int not null,
+    numero_emenda varchar(50) not null,
     
     primary key (sk_emenda)
 );
 
 create table dim_orgao (
-    sk_orgao int not null,
-    
-    cod_orgao_superior int not null,
-    nome_orgao_superior varchar(200) not null,
-    
-    cod_orgao_subordinado int not null,
-    orgao_subordinado varchar(200) not null,
+    sk_orgao smallint not null,
+    cod_orgao_superior varchar(50) not null,
+    nome_orgao_superior varchar(250) not null,
+    cod_orgao_subordinado varchar(50) not null,
+    orgao_subordinado varchar(250) not null,
     
     primary key (sk_orgao)
 );
+
+alter table dim_tempo modify column trimestre varchar(10);
+
+TRUNCATE TABLE dim_orgao;
+SELECT COUNT(*) FROM dim_orgao;
+
+select * from dim_orgao;
